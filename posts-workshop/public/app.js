3569d6e067253312f58eae3722a199c5d4e84aef
a25patel <aanaletap2510@gmail.com>
2015-10-07 12:42:39 -0600

    UI Routes set up and working

--- posts-workshop/public/app.js
+++ posts-workshop/public/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('posts-workshop', []); 
+var app = angular.module('posts-workshop', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('/', {
+      url: '/',
+      templateUrl: 'partials/posts.html',
+      controller: 'PostsController'
+    });
+});
